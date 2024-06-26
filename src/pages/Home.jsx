a69f61fab7d5a4e0db9f43fa669224bf7751b4e0
unknown <irsyadmuhammad954@gmail.com>
2024-06-26 07:54:57 -0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../component/Navbar";
+import Hero from "../component/Hero";
+import Featured from "../component/Featured";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Featured />
+    </>
+  );
+};
+
+export default Home;
